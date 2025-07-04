540d85a27eff342eafc2c215ce97c1dc44f93acd
Gurnav224 <chaudharyg856@gmail.com>
2025-07-04 12:04:19 +0530

    action commit 3

---
+++ example.js
@@ -0,0 +1,227 @@
+const data = {
+  ref: "refs/heads/master",
+  before: "d2f83cf8c42c14e004a5d32daa95a99b8edba876",
+  after: "686a7243ca6d0a32d2bf7c00b2c38dc042d11781",
+  repository: {
+    id: 1013494802,
+    node_id: "R_kgDOPGi0Eg",
+    name: "action-repo",
+    full_name: "Gurnav224/action-repo",
+    private: false,
+    owner: {
+      name: "Gurnav224",
+      email: "chaudharyg856@gmail.com",
+      login: "Gurnav224",
+      id: 72142482,
+      node_id: "MDQ6VXNlcjcyMTQyNDgy",
+      avatar_url: "https://avatars.githubusercontent.com/u/72142482?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/Gurnav224",
+      html_url: "https://github.com/Gurnav224",
+      followers_url: "https://api.github.com/users/Gurnav224/followers",
+      following_url:
+        "https://api.github.com/users/Gurnav224/following{/other_user}",
+      gists_url: "https://api.github.com/users/Gurnav224/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/Gurnav224/starred{/owner}{/repo}",
+      subscriptions_url: "https://api.github.com/users/Gurnav224/subscriptions",
+      organizations_url: "https://api.github.com/users/Gurnav224/orgs",
+      repos_url: "https://api.github.com/users/Gurnav224/repos",
+      events_url: "https://api.github.com/users/Gurnav224/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/Gurnav224/received_events",
+      type: "User",
+      user_view_type: "public",
+      site_admin: false,
+    },
+    html_url: "https://github.com/Gurnav224/action-repo",
+    description: null,
+    fork: false,
+    url: "https://api.github.com/repos/Gurnav224/action-repo",
+    forks_url: "https://api.github.com/repos/Gurnav224/action-repo/forks",
+    keys_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/keys{/key_id}",
+    collaborators_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/collaborators{/collaborator}",
+    teams_url: "https://api.github.com/repos/Gurnav224/action-repo/teams",
+    hooks_url: "https://api.github.com/repos/Gurnav224/action-repo/hooks",
+    issue_events_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/issues/events{/number}",
+    events_url: "https://api.github.com/repos/Gurnav224/action-repo/events",
+    assignees_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/assignees{/user}",
+    branches_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/branches{/branch}",
+    tags_url: "https://api.github.com/repos/Gurnav224/action-repo/tags",
+    blobs_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/git/blobs{/sha}",
+    git_tags_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/git/tags{/sha}",
+    git_refs_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/git/refs{/sha}",
+    trees_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/git/trees{/sha}",
+    statuses_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/statuses/{sha}",
+    languages_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/languages",
+    stargazers_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/stargazers",
+    contributors_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/contributors",
+    subscribers_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/subscribers",
+    subscription_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/subscription",
+    commits_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/commits{/sha}",
+    git_commits_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/git/commits{/sha}",
+    comments_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/comments{/number}",
+    issue_comment_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/issues/comments{/number}",
+    contents_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/contents/{+path}",
+    compare_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/compare/{base}...{head}",
+    merges_url: "https://api.github.com/repos/Gurnav224/action-repo/merges",
+    archive_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/{archive_format}{/ref}",
+    downloads_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/downloads",
+    issues_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/issues{/number}",
+    pulls_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/pulls{/number}",
+    milestones_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/milestones{/number}",
+    notifications_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/notifications{?since,all,participating}",
+    labels_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/labels{/name}",
+    releases_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/releases{/id}",
+    deployments_url:
+      "https://api.github.com/repos/Gurnav224/action-repo/deployments",
+    created_at: 1751595295,
+    updated_at: "2025-07-04T05:22:57Z",
+    pushed_at: 1751607512,
+    git_url: "git://github.com/Gurnav224/action-repo.git",
+    ssh_url: "git@github.com:Gurnav224/action-repo.git",
+    clone_url: "https://github.com/Gurnav224/action-repo.git",
+    svn_url: "https://github.com/Gurnav224/action-repo",
+    homepage: null,
+    size: 0,
+    stargazers_count: 0,
+    watchers_count: 0,
+    language: null,
+    has_issues: true,
+    has_projects: true,
+    has_downloads: true,
+    has_wiki: true,
+    has_pages: false,
+    has_discussions: false,
+    forks_count: 0,
+    mirror_url: null,
+    archived: false,
+    disabled: false,
+    open_issues_count: 0,
+    license: null,
+    allow_forking: true,
+    is_template: false,
+    web_commit_signoff_required: false,
+    topics: [],
+    visibility: "public",
+    forks: 0,
+    open_issues: 0,
+    watchers: 0,
+    default_branch: "master",
+    stargazers: 0,
+    master_branch: "master",
+  },
+  pusher: {
+    name: "Gurnav224",
+    email: "chaudharyg856@gmail.com",
+  },
+  sender: {
+    login: "Gurnav224",
+    id: 72142482,
+    node_id: "MDQ6VXNlcjcyMTQyNDgy",
+    avatar_url: "https://avatars.githubusercontent.com/u/72142482?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/Gurnav224",
+    html_url: "https://github.com/Gurnav224",
+    followers_url: "https://api.github.com/users/Gurnav224/followers",
+    following_url:
+      "https://api.github.com/users/Gurnav224/following{/other_user}",
+    gists_url: "https://api.github.com/users/Gurnav224/gists{/gist_id}",
+    starred_url:
+      "https://api.github.com/users/Gurnav224/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/Gurnav224/subscriptions",
+    organizations_url: "https://api.github.com/users/Gurnav224/orgs",
+    repos_url: "https://api.github.com/users/Gurnav224/repos",
+    events_url: "https://api.github.com/users/Gurnav224/events{/privacy}",
+    received_events_url:
+      "https://api.github.com/users/Gurnav224/received_events",
+    type: "User",
+    user_view_type: "public",
+    site_admin: false,
+  },
+  created: false,
+  deleted: false,
+  forced: false,
+  base_ref: null,
+  compare:
+    "https://github.com/Gurnav224/action-repo/compare/d2f83cf8c42c...686a7243ca6d",
+  commits: [
+    {
+      id: "686a7243ca6d0a32d2bf7c00b2c38dc042d11781",
+      tree_id: "456d8613e8b7d86b4068c053a606d133f6bff783",
+      distinct: true,
+      message: "second action",
+      timestamp: "2025-07-04T11:08:29+05:30",
+      url: "https://github.com/Gurnav224/action-repo/commit/686a7243ca6d0a32d2bf7c00b2c38dc042d11781",
+      author: {
+        name: "Gurnav224",
+        email: "chaudharyg856@gmail.com",
+        username: "Gurnav224",
+      },
+      committer: {
+        name: "Gurnav224",
+        email: "chaudharyg856@gmail.com",
+        username: "Gurnav224",
+      },
+      added: [],
+      removed: [],
+      modified: ["README.md"],
+    },
+  ],
+  head_commit: {
+    id: "686a7243ca6d0a32d2bf7c00b2c38dc042d11781",
+    tree_id: "456d8613e8b7d86b4068c053a606d133f6bff783",
+    distinct: true,
+    message: "second action",
+    timestamp: "2025-07-04T11:08:29+05:30",
+    url: "https://github.com/Gurnav224/action-repo/commit/686a7243ca6d0a32d2bf7c00b2c38dc042d11781",
+    author: {
+      name: "Gurnav224",
+      email: "chaudharyg856@gmail.com",
+      username: "Gurnav224",
+    },
+    committer: {
+      name: "Gurnav224",
+      email: "chaudharyg856@gmail.com",
+      username: "Gurnav224",
+    },
+    added: [],
+    removed: [],
+    modified: ["README.md"],
+  },
+};
+
+console.log(data.pusher.name);
+
+console.log(data.head_commit.id);
+
+console.table(data.ref.split("/").slice(-1)[0])
